87f454545f19b40420e923bcb566d55fe63f8d00
Brandon Hernández <isahecam@gmail.com>
2025-10-17 12:13:42 -0600

    feat(auth): add login form to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
+import { LoginForm } from "@/modules/auth/components/LoginForm";
 
 export default function Home() {
   return (
-    <main className='mx-auto max-w-4xl p-4'>
+    <main className='mx-auto grid min-h-screen max-w-4xl place-content-center items-center gap-y-12 p-4'>
       <h1 className='text-center text-xl font-bold text-purple-700'>
         Prueba Técnica — Frontend con Next.js
       </h1>
+
+      <section className='mx-auto w-full max-w-80'>
+        <LoginForm />
+      </section>
     </main>
   );
 }
