1fb419a362949fa88eefa4c85b208f64ab34d716
Brandon Hernández <isahecam@gmail.com>
2025-10-17 15:24:59 -0600

    feat(auth): make home page function async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { LoginForm } from "@/modules/auth/components/LoginForm";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className='mx-auto grid min-h-screen max-w-4xl place-content-center items-center gap-y-12 p-4'>
       <h1 className='text-center text-xl font-bold text-purple-700'>
