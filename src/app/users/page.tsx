db0473952f81c29571282d96b0f42cd4a8e5b9d6
Brandon Hernández <isahecam@gmail.com>
2025-10-17 20:30:22 -0600

    feat(users): create user page with protected access and user list header

---
+++ src/app/users/page.tsx
@@ -0,0 +1,13 @@
+export default async function Users() {
+  return (
+    <main className='mx-auto grid min-h-screen max-w-4xl place-content-center items-center gap-y-12 p-4'>
+      <h1 className='text-center text-xl font-bold text-purple-700'>
+        Página de Usuarios - (Página Protegida)
+      </h1>
+
+      <section className='mx-auto w-full max-w-80'>
+        <h3>Lista de Usuarios</h3>
+      </section>
+    </main>
+  );
+}
