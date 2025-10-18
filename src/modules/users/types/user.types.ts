4d500793dea0fb6955e56f82e10ad7e041048871
Brandon Hernández <isahecam@gmail.com>
2025-10-18 14:54:51 -0600

    feat(users): add UserListResponse interface for paginated user data

--- src/modules/users/types/user.types.ts
+++ src/modules/users/types/user.types.ts
@@ -26,3 +26,13 @@ export interface User {
   last_name: string;
   avatar: string;
 }
+
+export interface UserListResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: User[];
+  support: Support;
+  _meta: Meta;
+}
