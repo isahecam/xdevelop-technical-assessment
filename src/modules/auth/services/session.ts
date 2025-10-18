913b285587f42ae5b225b5ab7dac2336b29d2455
Brandon Hernández <isahecam@gmail.com>
2025-10-18 09:41:13 -0600

    refactor(auth): make session management stateless

--- src/modules/auth/services/session.ts
+++ src/modules/auth/services/session.ts
@@ -1,6 +1,22 @@
-import { decryptToken, encryptToken } from "@/lib/jwt";
+import "server-only";
+
+import { decrypt, encrypt } from "@/lib/jwt";
 import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
+
+export async function createSession(userId: string) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 días
+  const session = await encrypt({ userId, expiresAt });
+
+  const cookieStore = await cookies();
+
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
 
 /**
  * Función para obtener la sesión del usuario desde la cookie,
@@ -12,7 +28,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function getSession() {
   const session = (await cookies()).get("session")?.value;
   if (!session) return null;
-  return await decryptToken(session);
+  return await decrypt(session);
 }
 
 /**
@@ -20,29 +36,35 @@ export async function getSession() {
  * extendiendo la expiración del token JWT en la cookie,
  * como si fuera una renovación de un refresh token.
  *
- * @param request - NextRequest del usuario
  * @async
- * @returns NextResponse con la cookie actualizada
  */
-export async function updateSession(request: NextRequest) {
-  const session = request.cookies.get("session")?.value;
-  if (!session) return;
+export async function updateSession() {
+  const session = (await cookies()).get("session")?.value;
+  const payload = await decrypt(session);
 
-  // Decodificar el token y extender su expiración
-  const parsed = await decryptToken(session);
+  if (!session || !payload) return null;
 
-  // Crear una nueva fecha de expiración como si fuera un refresh token con 7 días de vida
-  parsed.expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 días
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 días
 
-  // Crear la respuesta con la cookie actualizada
-  const res = NextResponse.next();
+  const cookieStore = await cookies();
 
-  // Establecer la cookie con el token actualizado
-  res.cookies.set({
-    name: "session",
-    value: await encryptToken(parsed),
+  cookieStore.set("session", session, {
     httpOnly: true,
-    expires: parsed.expires,
+    secure: true,
+    expires: expires,
+    sameSite: "lax",
+    path: "/",
   });
-  return res;
+}
+
+/**
+ * Función para eliminar la sesión del usuario,
+ * eliminando la cookie de sesión.
+ *
+ * @async
+ * @return void
+ */
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
 }
