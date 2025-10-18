b3055595a8065c615e3df63ea07efe9deb5c7e9f
Brandon Hernández <isahecam@gmail.com>
2025-10-18 17:30:13 -0600

    feat(users): add getUser service for fetching user data

---
+++ src/modules/users/services/getUser.ts
@@ -0,0 +1,18 @@
+import { User, UserResponse } from "@/modules/users/types/user.types";
+
+export async function getUser(userId: User["id"]): Promise<User | null> {
+  // Obtener información pública del usuario a partir del userId
+  const user = await fetch(`https://reqres.in/api/users/${userId}`, {
+    method: "GET",
+    headers: {
+      "x-api-key": process.env.REQRES_API_KEY!,
+    },
+  });
+
+  if (!user.ok) {
+    return null;
+  }
+
+  const { data } = (await user.json()) as UserResponse;
+  return data;
+}
