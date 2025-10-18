2ea49c47887a402be4a772a792a7c01a94ca2c9a
Brandon Hernández <isahecam@gmail.com>
2025-10-18 17:29:28 -0600

    feat(users): add table columns configuration for user records

---
+++ src/modules/users/components/records/colums.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ColumnDef, FilterFn } from "@tanstack/react-table";
+import { User } from "@/modules/users/types/user.types";
+import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui";
+
+export const columns: ColumnDef<User>[] = [
+  {
+    header: "ID",
+    accessorKey: "id",
+    filterFn: "equalsString",
+  },
+  {
+    header: "Correo",
+    accessorKey: "email",
+    filterFn: "includesStringSensitive",
+  },
+  {
+    header: "Nombre completo",
+    accessorFn: row => `${row.first_name} ${row.last_name}`,
+  },
+  {
+    header: "Avatar",
+    accessorKey: "avatar",
+
+    cell: ({ row }) => {
+      return (
+        <Avatar>
+          <AvatarImage
+            src={row.getValue("avatar")}
+            alt="User's avatar"></AvatarImage>
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
+      );
+    },
+  },
+];
