2edc5dc574cf540ac7843895424fa887a7c472df
Brandon Hernández <isahecam@gmail.com>
2025-10-17 20:30:41 -0600

    feat(auth): add middleware for route protection and redirection

---
+++ src/modules/auth/services/middleware.ts
@@ -0,0 +1,26 @@
+import { getSession } from "./session";
+import { NextResponse, type NextRequest } from "next/server";
+
+// Especificación de rutas publicas y privadas
+const PROTECTED_ROUTES = ["/users"];
+const PUBLIC_ROUTES = ["/", "/login"];
+
+export async function middlewareAuth(request: NextRequest) {
+  const session = await getSession();
+
+  // Revisar si la ruta actual es protegida o pública
+  const path = request.nextUrl.pathname;
+
+  // Lógica de redirección basada en el estado de la sesión y la ruta
+  const isProtectedRoute = PROTECTED_ROUTES.includes(path);
+  const isPublicRoute = PUBLIC_ROUTES.includes(path);
+
+  // Redirigir según el estado de autenticación y la ruta solicitada
+  if (isPublicRoute && session) {
+    return NextResponse.redirect(new URL("/users", request.url));
+  }
+
+  if (isProtectedRoute && !session) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
