edcd6d664eeb3b2b3a1fd893fb5dd5fc79445ef3
Brandon Hernández <isahecam@gmail.com>
2025-10-19 15:30:04 -0600

    refactor(posts): enhance posts page with header and renamed component

--- src/app/(dashboard)/posts/page.tsx
+++ src/app/(dashboard)/posts/page.tsx
@@ -1,5 +1,13 @@
-import { Posts } from "@/modules/posts/components/Posts";
+import { PostList } from "@/modules/posts/components/PostList";
 
 export default async function PostsPage() {
-  return <Posts />;
+  return (
+    <section className='w-full space-y-5'>
+      <h2 className='text-lg font-bold text-purple-600'>
+        Todas las publicaciones
+      </h2>
+
+      <PostList />
+    </section>
+  );
 }
