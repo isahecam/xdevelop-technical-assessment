f5e941410d1f947f1e3816057489253f121326a0
Brandon Hernández <isahecam@gmail.com>
2025-10-19 12:05:14 -0600

    feat(users): display user role badge in profile component

--- src/modules/users/components/UserProfile.tsx
+++ src/modules/users/components/UserProfile.tsx
@@ -1,5 +1,5 @@
 import { LogoutButton } from "@/modules/auth/components/LogoutButton";
-import { Avatar, AvatarFallback, AvatarImage } from "@/shared//ui";
+import { Avatar, AvatarFallback, AvatarImage, Badge } from "@/shared//ui";
 import { User } from "../types/user.types";
 
 export function UserProfile({
@@ -7,7 +7,8 @@ export function UserProfile({
   last_name,
   avatar,
   email,
-}: Pick<User, "first_name" | "last_name" | "avatar" | "email">) {
+  role,
+}: Pick<User, "first_name" | "last_name" | "avatar" | "email" | "role">) {
   return (
     <article className='flex grow flex-col items-end'>
       <div className='flex items-center justify-end gap-x-4'>
@@ -15,7 +16,7 @@ export function UserProfile({
           <AvatarImage src={avatar} alt={`${first_name}'s Avatar`} />
           <AvatarFallback>{first_name.charAt(0)}</AvatarFallback>
         </Avatar>
-        <div>
+        <div className='flex flex-col items-end'>
           <p className='text-center text-xl text-gray-600'>
             Hola,{" "}
             <strong>
@@ -23,6 +24,9 @@ export function UserProfile({
             </strong>
           </p>
           <span>{email}</span>
+          <Badge variant={role?.id === "admin" ? "admin" : "user"}>
+            {role?.name}
+          </Badge>
         </div>
       </div>
       <LogoutButton />
