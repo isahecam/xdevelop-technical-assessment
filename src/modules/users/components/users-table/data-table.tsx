babbffd3be327341bae19ce9449335ef458894ce
Brandon Hernández <isahecam@gmail.com>
2025-10-18 18:16:08 -0600

    feat(users): translate empty state message to Spanish

--- src/modules/users/components/users-table/data-table.tsx
+++ src/modules/users/components/users-table/data-table.tsx
@@ -97,7 +97,7 @@ export function DataTable<TData, TValue>({
                 <TableCell
                   colSpan={columns.length}
                   className='h-24 text-center'>
-                  No results.
+                  No se encontraron resultados.
                 </TableCell>
               </TableRow>
             )}
