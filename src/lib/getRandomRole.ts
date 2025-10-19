fa56078089642949120cd0d0db8afeca443ff663
Brandon Hernández <isahecam@gmail.com>
2025-10-18 19:55:51 -0600

    refactor(lib): use shared Role type and export roles array

--- src/lib/getRandomRole.ts
+++ src/lib/getRandomRole.ts
@@ -1,14 +1,11 @@
-export interface Rol {
-  id: string;
-  name: string;
-}
+import { Role } from "@/modules/users/types/user.types";
 
-export function getRandomRole(): Rol {
-  const roles: Rol[] = [
-    { id: "admin", name: "Administrador" },
-    { id: "user", name: "Usuario" },
-  ];
+export const roles: Role[] = [
+  { id: "admin", name: "Administrador" },
+  { id: "user", name: "Usuario" },
+];
 
+export function getRandomRole(): Role {
   const randomRole = roles[Math.floor(Math.random() * roles.length)];
   return randomRole;
 }
