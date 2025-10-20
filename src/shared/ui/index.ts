8375a7153dbc8bbb1e12d79fafde6ca0226b6582
Brandon Hernández <isahecam@gmail.com>
2025-10-20 15:57:06 -0600

    feat(ui): export pagination component from shared UI module

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,6 +4,7 @@ export * from "./button";
 export * from "./card";
 export * from "./input";
 export * from "./label";
+export * from "./pagination";
 export * from "./select";
 export * from "./skeleton";
 export * from "./table";
