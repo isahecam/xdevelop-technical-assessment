93c1c71bdf3ff99842f845f05d066f6b4719eb43
Brandon Hernández <isahecam@gmail.com>
2025-10-20 23:56:16 -0600

    feat(auth): add /posts/favorites to protected routes

--- src/modules/auth/services/middleware.ts
+++ src/modules/auth/services/middleware.ts
@@ -2,7 +2,7 @@ import { getSession } from "./session";
 import { NextResponse, type NextRequest } from "next/server";
 
 // Especificación de rutas publicas y privadas
-const PROTECTED_ROUTES = ["/users", "/posts", "/books"];
+const PROTECTED_ROUTES = ["/users", "/posts", "/books", "/posts/favorites"];
 const PUBLIC_ROUTES = ["/", "/login"];
 
 export async function middlewareAuth(request: NextRequest) {
