c779f299a2419dd7155514bf425241a65dac79c1
Brandon Hernández <isahecam@gmail.com>
2025-10-20 21:23:28 -0600

    style(users): align UserProfile content to end with consistent text alignment

--- src/modules/users/components/UserProfile.tsx
+++ src/modules/users/components/UserProfile.tsx
@@ -10,14 +10,14 @@ export function UserProfile({
   role,
 }: Pick<User, "first_name" | "last_name" | "avatar" | "email" | "role">) {
   return (
-    <article className='flex grow flex-col items-end'>
+    <article className='flex grow flex-col items-end justify-end'>
       <div className='flex items-center justify-end gap-x-4'>
         <Avatar className='size-20'>
           <AvatarImage src={avatar} alt={`${first_name}'s Avatar`} />
           <AvatarFallback>{first_name.charAt(0)}</AvatarFallback>
         </Avatar>
         <div className='flex flex-col items-end'>
-          <p className='text-center text-xl text-gray-600'>
+          <p className='text-end text-xl text-gray-600'>
             Hola,{" "}
             <strong>
               {first_name} {last_name}
