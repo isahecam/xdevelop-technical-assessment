5c9ed779332bd51399aaf23b418cdf37ffaac6e2
Brandon Hernández <isahecam@gmail.com>
2025-10-20 23:44:37 -0600

    feat(posts): add favorite posts counter to favorites page

--- src/app/(dashboard)/posts/favorites/page.tsx
+++ src/app/(dashboard)/posts/favorites/page.tsx
@@ -1,11 +1,15 @@
+import { FavoritePostsCounter } from "@/modules/posts/components/FavoritePostsCounter";
 import { FavoritePostsList } from "@/modules/posts/components/FavoritePostsList";
 
 export default async function FavoritePostsPage() {
   return (
     <section className='w-full space-y-5'>
-      <h2 className='text-lg font-bold text-purple-600'>
-        Publicaciones Favoritas
-      </h2>
+      <div className='flex items-center justify-between'>
+        <h2 className='text-lg font-bold text-purple-600'>
+          Publicaciones Favoritas
+        </h2>
+        <FavoritePostsCounter />
+      </div>
 
       <FavoritePostsList />
     </section>
