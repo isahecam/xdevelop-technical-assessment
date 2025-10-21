7c2391f22e660a3dd7b7548d0e98692d6d62ec56
Brandon Hernández <isahecam@gmail.com>
2025-10-20 21:02:22 -0600

    feat(config): add remote image domains for Open Library and placeholder images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Dominios permitidos para las imágenes
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "covers.openlibrary.org",
+        port: "",
+        pathname: "/b/id/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
