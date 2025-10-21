1ba73b8e9ecf6670f94bf23b32b3102f7801fcf8
Brandon Hernández <isahecam@gmail.com>
2025-10-20 23:33:08 -0600

    feat(posts): add FavoritePostsList component

---
+++ src/modules/posts/components/FavoritePostsList.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { PostCard } from "@/modules/posts/components/PostCard";
+import { useFavoritePostsStore } from "../store/favoritePosts.store";
+
+export function FavoritePostsList() {
+  const { favoritePosts } = useFavoritePostsStore();
+
+  if (favoritePosts.length === 0)
+    return <div>No hay publicaciones favoritas.</div>;
+
+  return (
+    <ul className='grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-3'>
+      {favoritePosts.map(post => (
+        <li key={post.id} className='contents'>
+          <PostCard {...post} />
+        </li>
+      ))}
+    </ul>
+  );
+}
